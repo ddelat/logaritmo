6128340fbab02ae22a31eeafc0dfe0acc29727fc
ddelat <ddelatorrevaldezate@gmail.com>
2024-04-18 11:26:00 +0200

    funcion logaritmo

---
+++ logaritmo/log.py
@@ -0,0 +1,14 @@
+import math
+
+
+def calcular_logaritmo_neperiano(numero):
+    return math.log(numero)
+
+
+# Ejemplo de uso
+numero = 10
+resultado = calcular_logaritmo_neperiano(numero)
+print(f"El logaritmo neperiano de {numero} es: {resultado}")
+numero = 10
+resultado = calcular_logaritmo_neperiano(numero)
+print(f"El logaritmo neperiano de {numero} es: {resultado}")
