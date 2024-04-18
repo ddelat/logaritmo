4fa439303ca5be25c0232f79953c51fb565ac3fe
ddelat <ddelatorrevaldezate@gmail.com>
2024-04-18 11:38:10 +0200

    actualizacion error

--- logaritmo/log.py
+++ logaritmo/log.py
@@ -2,10 +2,15 @@ import math
 
 
 def calcular_logaritmo_neperiano(numero):
-    return math.log(numero)
+    if numero > 0:
+        return math.log(numero)
+    else:
+        print('Error en el numero')
 
 
 # Ejemplo de uso
 numero = 10
 resultado = calcular_logaritmo_neperiano(numero)
 print(f"El logaritmo neperiano de {numero} es: {resultado}")
+numero = -1
+resultado = calcular_logaritmo_neperiano(numero)
